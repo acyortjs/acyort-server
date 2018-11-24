c4a2e770b1247985de2e1145b936be64bc00b0a2
LoeiFy <LoeiFy@gmail.com>
2018-11-24 20:14:40 +0800

    refactor

---
+++ index.js
@@ -0,0 +1,44 @@
+const { join, extname } = require('path')
+const Pavane = require('pavane')
+
+const base = process.cwd()
+const watches = join(base, 'templates')
+const publics = base
+const server = new Pavane(watches, publics)
+
+module.exports = (acyort) => {
+  acyort.cli.register('commands', {
+    name: 'server',
+    fullName: 'server',
+    description: 'LiveReload Server',
+    action: async (argv, _acyort) => {
+      server.listener = async (args) => {
+        const {
+          event,
+          path,
+          message,
+          reloadCss,
+          reloadPage,
+        } = args
+
+        if (event === 'info') {
+          _acyort.logger.info(message)
+          return
+        }
+
+        _acyort.logger.info(`${event}: ${path.split(`${base}/templates/`)[1]}`)
+
+        await _acyort.process()
+
+        if (extname(path) === '.css') {
+          reloadCss()
+        } else {
+          reloadPage()
+        }
+      }
+
+      await _acyort.process()
+      server.start(2222)
+    },
+  })
+}
---
+++ test/scripts/script.js
@@ -0,0 +1,24 @@
+module.exports = (acyort) => {
+  function d() {
+    acyort.outputHTML({
+      template: 'index',
+      path: 'index.html',
+      data: {
+        zero: 0,
+        one: 1,
+        other: 10,
+      },
+    })
+    acyort.copySource()
+  }
+
+  acyort.store.set('key', 5)
+
+  acyort.workflow.register(d)
+
+  acyort.helper.register('_h5', function h5() {
+    const key = acyort.store.get('key')
+    const number = this.one
+    return `This is h${key}, not h${number}`
+  })
+}
